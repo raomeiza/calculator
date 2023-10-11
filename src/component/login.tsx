4c57244d614ad8d0246dce17401bac9d7ccc386b
a omeiza <37030673+raomeiza@users.noreply.github.com>
2023-10-11 18:29:08 +0100

    created login and history slides
    - login slide
      - handles both login and signup
      - shows when user is not signed in
    
    - history slide
      - shows when user is signed in
      - shows history of calculations
      - shows history of user's calculations
    
    -  icons.svg
      generates svg icons from svg files in public/svg folder
    
    -  SlideContainer.tsx
      - handles slide transitions
      - handles slide state

---
+++ src/component/login.tsx
@@ -0,0 +1,137 @@
+import * as React from 'react';
+import Button from '@mui/material/Button';
+import { Box, Divider, IconButton, TextField, Typography, Slide } from '@mui/material';
+import { UserContext } from '../context/userContext';
+import { ArrowDown, History } from './svg/icons';
+import SlideContainer from './SlideContainer';
+export default function Login(props: any) {
+	const { open, setOpen } = props;
+	const { user, login } = React.useContext(UserContext);
+	const [isRegistered, setIsRegistered] = React.useState(true);
+	const [name, setName] = React.useState('');
+	const [email, setEmail] = React.useState('');
+	const [password, setPassword] = React.useState('');
+	const [repeatPassword, setRepeatPassword] = React.useState('');
+	const [isLoading, setIsLoading] = React.useState(false);
+	const [errorMessage, setErrorMessage] = React.useState('');
+
+
+
+
+	const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+		let url;
+		let data: { email: string; password: string; name?: string; repeatPassword?: string };
+		event.preventDefault();
+		if (isRegistered) {
+			url = 'http://localhost:5000/user/login';
+			data = { email, password };
+		} else {
+			data = { name, email, password, repeatPassword };
+			url = 'http://localhost:5000/user/register';
+		}
+		// make simple validation
+		if (!email || !password) {
+			setErrorMessage('Please fill in all fields');
+			return;
+		}
+		if (!isRegistered && password !== repeatPassword) {
+			setErrorMessage('Passwords do not match');
+			return;
+		}
+		setIsLoading(true);
+		fetch(url, {
+			method: 'POST',
+			headers: {
+				'Content-Type': 'application/json',
+			},
+			body: JSON.stringify(data),
+		})
+			.then((res) => res.json())
+			.then((data) => {
+				if (!data.success) { throw data }
+				const { user, token } = data.data;
+					setIsLoading(false);
+					login(user.email, token);
+			})
+			.catch((err) => {
+				setIsLoading(false);
+				setErrorMessage(err.message);
+			});
+	};
+
+	return (
+		<SlideContainer open={open} setOpen={setOpen}>
+				<Box
+					sx={{
+						display: 'flex',
+						flexDirection: 'column',
+						justifyContent: 'center',
+						alignItems: 'center',
+						gap: 2,
+						width: '100%',
+						height: open ? '480px' : 0,
+					}}
+					component={'form'}
+			onSubmit={handleSubmit}
+				>
+					<Typography variant="h5" >{isRegistered ? 'Login' : 'Register'}</Typography>
+					<TextField
+						label="Email"
+						variant="outlined"
+						fullWidth
+						type="email"
+						value={email}
+						onChange={(event) => setEmail(event.target.value)}
+						required
+					/>
+					{!isRegistered && (
+						<TextField
+							label="Name"
+							variant="outlined"
+							fullWidth
+							type="text"
+							value={name}
+							onChange={(event) => setName(event.target.value)}
+							required
+						/>
+					)}
+					<TextField
+						label="Password"
+						variant="outlined"
+						fullWidth
+						type="password"
+						value={password}
+						onChange={(event) => setPassword(event.target.value)}
+						required
+					/>
+
+					{!isRegistered && (
+						<TextField
+							label="Confirm Password"
+							variant="outlined"
+							fullWidth
+							type="password"
+							value={repeatPassword}
+							onChange={(event) => setRepeatPassword(event.target.value)}
+							required
+						/>
+					)}
+					{errorMessage && (
+						<Typography variant="body2" sx={{ color: 'red', mt: 2 }}>
+							{errorMessage}
+						</Typography>
+					)}
+					<Button disabled={isLoading} type="submit" variant="contained" sx={{ mt: 2 }}>
+						{isRegistered ? 'Login' : 'Register'}
+					</Button>
+					<Divider sx={{ my: 2, width: '100%' }} />
+					<Typography variant="body2">
+						{isRegistered ? "Don't have an account?" : 'Already have an account?'}
+						<Button sx={{ ml: 1 }} onClick={() => setIsRegistered(!isRegistered)}>
+							{isRegistered ? 'Register' : 'Login'}
+						</Button>
+					</Typography>
+				</Box>
+		</SlideContainer>
+	);
+}
---
+++ src/component/svg/icons.tsx
@@ -0,0 +1,23 @@
+import { SvgIcon } from "@mui/material";
+
+export function ArrowDown() {
+    return (
+        <SvgIcon>
+            <svg className="MuiSvgIcon-root MuiSvgIcon-fontSizeMedium MuiSvgIcon-root MuiSvgIcon-fontSizeLarge css-zjt8k" focusable="false" aria-hidden="true" viewBox="0 0 24 24" ><path d="M7.41 8.59 12 13.17l4.59-4.58L18 10l-6 6-6-6 1.41-1.41z"></path></svg>
+        </SvgIcon>
+    )
+}
+export function DoubleArrowDown() {
+    return (
+        <SvgIcon>
+            <svg className="MuiSvgIcon-root MuiSvgIcon-fontSizeMedium MuiBox-root css-1om0hkc" focusable="false" aria-hidden="true" viewBox="0 0 24 24" data-testid="KeyboardDoubleArrowDownIcon"><path d="M18 6.41 16.59 5 12 9.58 7.41 5 6 6.41l6 6z"></path><path d="m18 13-1.41-1.41L12 16.17l-4.59-4.58L6 13l6 6z"></path></svg>
+        </SvgIcon>
+    )
+}
+export function History() {
+    return (
+        <SvgIcon>
+            <svg className="MuiSvgIcon-root MuiSvgIcon-fontSizeMedium MuiBox-root css-1om0hkc" focusable="false" aria-hidden="true" viewBox="0 0 24 24" data-testid="HistoryIcon"><path d="M13 3c-4.97 0-9 4.03-9 9H1l3.89 3.89.07.14L9 12H6c0-3.87 3.13-7 7-7s7 3.13 7 7-3.13 7-7 7c-1.93 0-3.68-.79-4.94-2.06l-1.42 1.42C8.27 19.99 10.51 21 13 21c4.97 0 9-4.03 9-9s-4.03-9-9-9zm-1 5v5l4.28 2.54.72-1.21-3.5-2.08V8H12z"></path></svg>
+        </SvgIcon>
+    )
+}
